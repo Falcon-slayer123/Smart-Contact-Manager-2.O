d83701c88885878825bc2699279f4a09074fd93a
Falcon-slayer123 <himanshutrivedi133@gmail.com>
2024-07-01 14:26:31 +0530

    Added User Entity and Provider Enum

---
+++ SCM2.O/src/main/java/com/scm/Application.java
@@ -0,0 +1,19 @@
+package com.scm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	{
+		System.out.println("**********SCM2.0 STARTS*********");
+	}
+
+
+
+}
